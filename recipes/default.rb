9079bfcb1ca95de62eb110a294bd778a08d971b9
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-08-20 14:36:57 +0300

    Use yum-epel recipe.
    Fixes bug when EPEL6 is used on CentOS7

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,13 +18,7 @@
 #
 
 
-yum_repository 'epel' do
-  description 'Extra Packages for Enterprise Linux'
-  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-  action :create
-  only_if { platform_family?('rhel') }
-end
+include_recipe "yum-epel" if platform_family?('rhel')
 
 package 'lsyncd'
 
