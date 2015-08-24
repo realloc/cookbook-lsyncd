d87f0f04196d174db47d4bcdf4fa274e789e8142
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-08-24 19:32:46 +0300

    Try adding rsync option

--- resources/target.rb
+++ resources/target.rb
@@ -7,6 +7,7 @@ attribute :target, :kind_of => String, :required => true
 attribute :user, :kind_of => [String, NilClass]
 attribute :host, :kind_of => [String, NilClass]
 attribute :rsync_opts, :kind_of => [Array, NilClass]
+attribute :rsync, :kind_of => [Hash, NilClass]
 attribute :exclude, :kind_of => [Array, NilClass]
 attribute :exclude_from, :kind_of => [String, NilClass]
 attribute :cookbook, :kind_of => String, :default => 'lsyncd'
